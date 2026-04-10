38cc43800f06dca1c967c7ba12d475c3debefa8d
adtzslowy <aditzkun0987@gmail.com>
2026-04-10 15:51:22 +0700

    Initial Commit

---
+++ GoUI/GoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoUIApp.swift
+//  GoUI
+//
+//  Created by ADITYA PRASETYO on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct GoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
